463996bfdf62bd0697ecae3389d66c6af3903d3f
Mat. Hdez <mateohdez950@gmail.com>
2020-12-16 09:39:34 -0600

    recorrido corregido
    ahora correctamente recorre todos los elemntos del arbol con n hojas

--- arbol.js
+++ arbol.js
@@ -13,11 +13,11 @@ const n2 = new Nodo('TITLE');
 let n3 = new Nodo('BODY');
 const n4 = new Nodo('DIV');
 const n5 = new Nodo('CANVAS');
-const n6 = new Nodo('BUTTON');
-const n7 = new Nodo('BUTTON');
-const n8 = new Nodo('BUTTON');
-const n9 = new Nodo('BUTTON');
-const n10 = new Nodo('BUTTON');
+const n6 = new Nodo('BUTTON1');
+const n7 = new Nodo('BUTTON2');
+const n8 = new Nodo('BUTTON3');
+const n9 = new Nodo('BUTTON4');
+const n10 = new Nodo('BUTTON5');
 
 // ejemplo de la pagina de linea simple original
 n0.hijos.push(n1,n3);//html es padre de head y body
@@ -26,15 +26,15 @@ n3.hijos.push(n4);//body contiene a div y 5 botones
 n3.hijos.push(n6,n7,n8,n9,n10);
 n4.hijos.push(n5);//div contiene a canvas
 
-function preOrderTraversal(node = this.root,i) {
+function preOrderTraversalN(node = this.root,i) {
 //el recorrido clasico de preorden no funciona aqui, ya que pueden ser mas de 2 hijos   
-    console.log(node.dato);//imprimo al padre
-    while(node.hijos[i])
+    
+    while(node.hijos[i])//mientras tenga hijos que visitar
     {
-      console.log(node.hijos[i].dato);
-      preOrderTraversal(node.hijos[i],i++);
+      console.log(node.hijos[i].dato);//muestralo
+      preOrderTraversalN(node.hijos[i],0);
+      i++;
     }
-   
 }
 
-preOrderTraversal(n0,0);
+preOrderTraversalN(n0,0);
